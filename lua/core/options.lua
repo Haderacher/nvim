aa684368ff210ab007a5a39a8ba2228198ef2a23
haderacher <liu235991@gmail.com>
2026-01-13 11:25:07 +0800

    feat: 将行号宽度设置为4

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -8,6 +8,7 @@ opt.signcolumn = "yes:2"
 opt.relativenumber = true -- 相对行号（极力推荐，方便跳转）
 opt.number = true         -- 显示当前行号
 opt.wrap = false
+opt.numberwidth = 4
 
 -- 缩进
 opt.tabstop = 4           -- 1个 Tab 等于 4 个空格
