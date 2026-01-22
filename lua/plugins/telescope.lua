f382f5d0284bbf3e4a32204a54993dac222bfe0f
haderacher <liu235991@gmail.com>
2026-01-22 12:07:05 +0800

    refactor: format rainbow-delimiters.lua and telescope.lua

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,8 +1,8 @@
 return {
-    'nvim-telescope/telescope.nvim',
-    dependencies = {
-        'nvim-lua/plenary.nvim',
-        -- optional but recommended
-        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-    }
+	"nvim-telescope/telescope.nvim",
+	enable = false,
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+	},
 }
